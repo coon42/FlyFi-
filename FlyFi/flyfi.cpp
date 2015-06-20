7538c88cd6f03465a97edb4fc29e59d6a317a981
coon <coon@c-base.org>
2015-06-20 22:48:28 +0200

    Created a Qt based gui and added a serial library

---
+++ FlyFi/flyfi.cpp
@@ -0,0 +1,116 @@
+#include "flyfi.h"
+
+FlyFi::FlyFi(QWidget *parent) : QMainWindow(parent) {
+    ui.setupUi(this);
+}
+
+FlyFi::~FlyFi() {
+
+}
+
+void FlyFi::showEvent(QShowEvent* event) {
+  QWidget::showEvent(event);
+
+  listSerialPorts();
+  addBaudRates();
+}
+
+void FlyFi::setFloatNum(float float_num) {
+  char freq[256];
+  sprintf(freq, "%.2f Hz", float_num / 100.0f);
+  ui.lblFreq->setText(freq);
+}
+
+void FlyFi::on_btnRefreshPorts_clicked() {
+  ui.cmbSerialPorts->clear();
+  availSerPorts_.clear();
+
+  listSerialPorts();
+}
+
+void FlyFi::on_btnOpen_clicked() {
+  int baudRate = ui.cmbBaudrate->currentText().toInt();
+  string portName = availSerPorts_[ui.cmbSerialPorts->currentIndex()].port;
+
+  ser_.close();
+  ser_.setPort(portName);
+  ser_.setBaudrate(baudRate);
+  ser_.open();
+
+  if (ser_.isOpen())
+    dbg("connected to serial port %s", ser_.getPort().c_str());
+}
+
+void FlyFi::on_sldFreq_valueChanged(int val) {
+  setFloatNum(val);
+}
+
+void FlyFi::on_btnPlay_clicked() {
+  if (ser_.isOpen()) {
+    ser_.write("Play");
+    dbg("Play");
+  }
+  else
+    dbg("Ser Port is not open!");
+    
+  //recvThread_.start();
+}
+
+void FlyFi::on_btnStop_clicked() {
+  if (ser_.isOpen()) {
+    ser_.write("Stop");
+    dbg("Stop");
+  }
+  else
+    dbg("Ser Port is not open!");
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// Menu bar
+// ------------------------------------------------------------------------------------------------------------
+
+void FlyFi::on_action_close_triggered() {
+  this->close();
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// helpers
+// ------------------------------------------------------------------------------------------------------------
+
+void FlyFi::dbg(string format, ...) {
+  char formattedText[512];
+
+  va_list args;
+  va_start(args, format);
+  vsnprintf_s(formattedText, sizeof(formattedText), sizeof(formattedText), format.c_str(), args);
+  va_end(args);
+
+  ui.edtDebug->append(formattedText);
+}
+
+void FlyFi::listSerialPorts() {
+  availSerPorts_ = serial::list_ports();
+
+  char deviceName[128];
+  for(auto pDevice = availSerPorts_.begin(); pDevice != availSerPorts_.end(); pDevice++) {
+    sprintf_s(deviceName, sizeof(deviceName), "%s - %s", pDevice->port.c_str(), pDevice->description.c_str());
+    ui.cmbSerialPorts->addItem(deviceName);
+  }
+}
+
+void FlyFi::addBaudRates() {
+  ui.cmbBaudrate->addItem("300");
+  ui.cmbBaudrate->addItem("1200");
+  ui.cmbBaudrate->addItem("2400");
+  ui.cmbBaudrate->addItem("4800");
+  ui.cmbBaudrate->addItem("9600");
+  ui.cmbBaudrate->addItem("14400");
+  ui.cmbBaudrate->addItem("19200");
+  ui.cmbBaudrate->addItem("28800");
+  ui.cmbBaudrate->addItem("38400");
+  ui.cmbBaudrate->addItem("57600");
+  ui.cmbBaudrate->addItem("115200");
+  ui.cmbBaudrate->addItem("230400");
+
+  ui.cmbBaudrate->setCurrentIndex(4);
+}
---
+++ FlyFi/flyfi.h
@@ -0,0 +1,56 @@
+#ifndef FLYFI_H
+#define FLYFI_H
+
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMessageBox>
+#include <QList>
+#include <QThread>
+#include <vector>
+#include "serial/serial.h"
+#include "ui_flyfi.h"
+
+using namespace std;
+
+class SerialRecvThread : public QThread {
+private:
+  void run(Ui::FlyFiClass* ui, serial::Serial* pSer) {
+    while (pSer->isOpen()) {
+      pSer->waitReadable();
+      std::string data = pSer->read();      
+      ui->edtDebug->append(QString::fromStdString(data));
+    }
+  }
+};
+
+class FlyFi : public QMainWindow {
+    Q_OBJECT
+
+public:
+    FlyFi(QWidget *parent = 0);
+    ~FlyFi();
+    void FlyFi::showEvent(QShowEvent* event);
+
+private:
+  Ui::FlyFiClass ui;
+  serial::Serial ser_;
+  SerialRecvThread recvThread_;
+  vector<serial::PortInfo> availSerPorts_ = serial::list_ports();
+
+  // helpers
+  void listSerialPorts();
+  void addBaudRates();
+  void setFloatNum(float float_num);
+  void dbg(string format, ...);
+  
+public slots:
+  void on_btnRefreshPorts_clicked();
+  void on_btnOpen_clicked();
+  void on_sldFreq_valueChanged(int val);
+  void on_btnPlay_clicked();
+  void on_btnStop_clicked();
+  void on_action_close_triggered();
+};
+
+
+
+#endif // FLYFI_H
